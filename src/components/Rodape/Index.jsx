ff00421490d0ade118231a664cbf8a7c916702de
Leonardo <leonardofeitos4@gmail.com>
2025-08-18 16:07:25 -0300

    Atualização Home em geral

--- src/components/Rodape/Index.jsx
+++ src/components/Rodape/Index.jsx
@@ -1,39 +1,40 @@
 import './Style.css';
 
+import FacebookIcon from '../../assets/icon/facebook_icone.png';
+import InstagramIcon from '../../assets/icon/instagram_icone.png';
+import WhatsappIcon from '../../assets/icon/whatsapp_icone.png';
+import YoutubeIcon from '../../assets/icon/youtube_4494485.png';
+
 function Rodape() {
   return (
     <footer className="rodape">
       <div className="rodape-conteudo">
 
-        {/* Coluna esquerda - Contato */}
         <div className="contato">
           <h3>Contato</h3>
           <p>R. Antônio Pádua Vasconcelos, 53 - Cristo Redentor, João Pessoa - PB, 58071-400</p>
           <p>FranternidadeFilhasdeSantaClara@gmail.com</p>
           <p>📞 (83) 9918-5311</p>
 
-          {/* Ícones sociais (clicáveis) */}
           <div className="icons">
             <a href="https://www.facebook.com/people/Fraternidade-filhas-de-Santa-Clara/61561085580958/?rdid=w4LwqPVjXQ9NFhoK&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F1GgWSgEv7z%2F" 
-            target="_blank" rel="noreferrer">
-              <img src="/src/assets/icon/facebook_icone.png" alt="Facebook" />
+               target="_blank" rel="noreferrer">
+              <img src={FacebookIcon} alt="Facebook" />
             </a>
             <a href="https://www.instagram.com/fraternidadefsc/" 
-            target="_blank" rel="noreferrer">
-              <img src="/src/assets/icon/instagram_icone.png" alt="Instagram" />
+               target="_blank" rel="noreferrer">
+              <img src={InstagramIcon} alt="Instagram" />
             </a>
             <a href="https://api.whatsapp.com/send/?phone=5583991853511&text&type=phone_number&app_absent=0&wame_ctl=1&source_surface=23" 
-            target="_blank" rel="noreferrer">
-              <img src="/src/assets/icon/whatsapp_icone.png" alt="WhatsApp" />
+               target="_blank" rel="noreferrer">
+              <img src={WhatsappIcon} alt="WhatsApp" />
             </a>
             <a href="https://www.youtube.com/@fraternidadefsc" 
-            target="_blank" rel="noreferrer">
-              <img src="/src/assets/icon/youtube_4494485.png" alt="Youtube" />
+               target="_blank" rel="noreferrer">
+              <img src={YoutubeIcon} alt="Youtube" />
             </a>
           </div>
         </div>
-
-        
 
       </div>
     </footer>
