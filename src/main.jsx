3714a798003197c00679fc86df7a3a8e8b5c8dbb
daniel_pontes <dpvirginio@gmail.com>
2025-08-18 14:54:11 -0300

    subindo topo do site

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Topo from './components/topo'
-import Home from './pages/Home'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Home />
-    <Topo />
-  </StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./index.css";
+
+import Topo from "./components/Topo/Index.jsx";
+import Home from "./pages/Home/Index.jsx";
+
+import Institucional from "./pages/Institucional/Index.jsx";
+import NossosProjetos from "./pages/NossosProjetos/Index.jsx";
+import NossaMissao from "./pages/NossaMissao/Index.jsx";
+import Doador from "./pages/Doador/Index.jsx";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Topo />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Institucional" element={<Institucional />} />
+        <Route path="/NossosProjetos" element={<NossosProjetos />} />
+        <Route path="/NossaMissao" element={<NossaMissao />} />
+        <Route path="/Doador" element={<Doador />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
