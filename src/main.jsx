c77158743d49320c55ca161a11e40621da25e57f
leonardo feitosa barroso <leonardofeitos4@gmail.com>
2026-05-02 16:57:27 -0300

    att

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import "./index.css";
 import Topo from "./components/Topo/Index.jsx";
